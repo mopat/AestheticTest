18983e5d80f1fac43bd8b6348c27a8b56f601060
mopat <morczinietz@gmail.com>
2015-03-05 18:17:09 +0100

    TestSetup to read url and time

--- src/TestSetup.js
+++ src/TestSetup.js
@@ -1,12 +1,25 @@
 /**
  * Created by Patrick on 05.03.2015.
  */
-AestheticTest.TestSetup = (function() {
+AestheticTest.TestSetup = (function () {
     var that = {},
+        testUrl = null,
+        time = null,
 
-        init = function() {
+        init = function () {
             console.log("init testsetup");
+
+            testUrl = getParameterByName("testurl");
+            time = getParameterByName("time");
+
             return this;
+        },
+
+        getParameterByName = function (name) {
+            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                results = regex.exec(location.search);
+            return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
         };
 
     that.init = init;
