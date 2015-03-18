85fb6576e08071ac0349ee9ee69004d1becd88d9
ma-jo-ne <jonas.massinger@gmail.com>
2015-03-18 20:12:43 +0100

    Parse Daten in Tabelle anzeigen

--- src/Result.js
+++ src/Result.js
@@ -1,20 +1,34 @@
-AestheticTest.Result = (function () {
-    var that = {},
-
-
-        init = function () {
 
 
             // Initialize Parse with your Parse application javascript keys
             Parse.initialize("f9adAlRbVFDK1YlOeuU5sbeIi6e46brSVvADAUZW",
                 "Y9hZUmuVX5EHU7q05rdsO7CuaOQNH1XxZ0K5IWk1");
 
+            $("#test-result-button").click(function () {
+                showResults();
+            });
 
-            return this;
-        };
+            showResults = function () {
+                var teamName = $("#projectname-input").val() + "_table";
+                var team = Parse.Object.extend(teamName);
+                var query = new Parse.Query(team);
 
-    that.init = init;
+                query.find({
+                    success: function (results) {
+                        // Do something with the returned Parse.Object values
+                        for (var i = 0; i < results.length; i++) {
+                            var object = results[i];
 
 
-    return that;
-}());
+                            $("#player-table").append($("<tr class='player-context-menu'>").append($('<td></td>'
+                            + '<td class="player-name">' + object.get('ghghgText') + '</td>')));
+                        }
+                    },
+                    error: function (error) {
+                        alert("Error: " + error.code + " " + error.message);
+                    }
+                });
+
+            };
+
+
