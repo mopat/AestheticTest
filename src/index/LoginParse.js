52ca1fed3aadebf83c59b1d9d5a67dee2f5a5208
mopat <morczinietz@gmail.com>
2015-05-18 16:50:06 +0200

    Beta FInal!

--- src/index/LoginParse.js
+++ src/index/LoginParse.js
@@ -2,7 +2,6 @@ AestheticIndex.LoginParse = (function () {
     var that = {},
 
         init = function () {
-
             Parse.initialize("f9adAlRbVFDK1YlOeuU5sbeIi6e46brSVvADAUZW",
                 "Y9hZUmuVX5EHU7q05rdsO7CuaOQNH1XxZ0K5IWk1");
 
